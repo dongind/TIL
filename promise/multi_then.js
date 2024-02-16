0a8d546060365231282d1dfc891d86f56d2ba4a7
dongind.oct <dongind.oct@gmail.com>
2024-02-16 17:59:54 +0900

    promise : then 안에 setTimeout 넣기
    1. 첫번째 then 안에 setTimeout을 넣어서 동작 상태를 확인
    => 확인 결과, 모든 then을 수행하고 나서, setTimeout의 콜백 함수가 실행됨.
    
    2. 위의 동작으로 유추할 수 있는 부분은 setTimeout의 콜백함수가 callback queue로 들어가기 전에, 이미 then의 함수들이 callback queue에 들어가 있다고 유추할 수 있다. 해당 내용을 추가적으로 공식 문서에서 확인할 필요가 있음

--- promise/multi_then.js
+++ promise/multi_then.js
@@ -1,12 +1,29 @@
 const testPromise = new Promise((resolve) => {
   console.log("In promise executor");
-  resolve("Promise!");
+  setTimeout(() => {
+    resolve("Promise!");
+  }, 0);
 });
 
-testPromise.then((result) => {
-  console.log(result, "1");
-});
+testPromise
+  .then((result) => {
+    console.log(result, "1");
+    setTimeout(() => {
+      console.log("it's in setTimeout of first then method");
+      return result;
+    }, 0);
+  })
+  .then((result) => {
+    console.log(result, "2");
+    return result;
+  });
 
-testPromise.then((result) => {
-  console.log(result, "2");
-});
+testPromise
+  .then((result) => {
+    console.log(result, "3");
+    return result;
+  })
+  .then((result) => {
+    console.log(result, "4");
+    return result;
+  });
